b97858be28b49f78f8c3f59370cc65e5c96bfae5
CozyHome <gaunletgames@gmail.com>
2023-10-30 01:15:23 -0700

    planned refactor of .glTF importer alongside reconsideration of how scenes are loaded.

--- js/scene.js
+++ js/scene.js
@@ -67,10 +67,86 @@ export class scene_transitive {
 export class scene_context {
 	#_mesh_registry; // what meshes are stored in this gltf file
 	#_image_registry; // what images are stored in this gltf file
-	#_tex_registry;
+	#_texture_registry; // what textures are stored in gltf file (bitmap, sampler)
+
+/*
+ * I'd like to refactor this implementation entirely. It seems like we have
+ * the following acyclic dependency list for objects in the scene:
+ *
+ * Materials:
+ * 	-> Textures
+ *
+ * Textures:
+ * 	-> Samplers (GPUSamplers/NativeSamplers)
+ * 	-> Images (Bitmaps/Blobs/etc)
+ *
+ * Meshes:
+ * 	-> Vertex Buffers
+ * 	-> Index Buffers
+ * 	-> Material Indices
+ * 	-> Skinning Indices
+ *
+ *	Node:
+ *		Transformation Matrix:
+ *		Mesh:
+ *			Material:
+ *				Shader: (implicitly constructed via Material)
+ *				Texture:
+ *					Image:
+ *					Sampler
+ *			Vertex Buffer(s)
+ *			Index Buffer(s)
+ *
+ *		TODO: Skinning Code (...)	
+ *
+ * 	Objects in the .gltf scene are stored in run-length format
+ * 	in their respective sublists: (nodes, meshes, materials, textures, etc.)
+ *
+ *	Obviously, the most straightforward approach to deserializing a .gtlf
+ *	scene is bottom up. Determine what elements in the scene are not dependent
+ *	on anybody else. This is usually data like images, meshes, etc.
+ *
+ *	(Read GLTF File) 0. read the .gltf file and successfully convert it into a JSON object.
+ *
+ *  (Import Bitmaps) 1. index all images into bitmaps and upload to the GPU.
+ *  (Import Samplers) 2. index all samplers into GPUSamplers and upload to the GPU.
+ *
+ *  (Construct NativeTextures) 3. construct a list of native textures via the prior two lists.
+ *
+ * 	(Import Meshes) 4. index all meshes and upload their vertex attribute buffers to the GPU.
+ * 		-> Implement the ability to select between sparse-length versus object-length encoding
+ * 		for the attribute buffers in the vertex buffer.
+ *
+ * 		Basically, have a translation layer that takes the structure in which .gltf files
+ * 		store vertex attribute information (sparse-length) and let the user control how it should
+ * 		be uploaded to the GPU.
+ *
+ * <-- WE ARE CURRENTLY HERE @ 10/30/23 -->
+ * I want to refactor steps (1-4) with as much clarity as I can afford using a text-editor like Vim
+ * and my spare time off of work.
+ *
+ * (Import Materials) 5. This is very ambiguous at the moment as I do not know the various configurations
+ * that materials can take shape in. Materials should obviously come after step 3, though.
+ *
+ * (Build Scene Graph) 6. At the moment, compute_scene_hierarchy build a scene graph using the TRS-model
+ * and can successfully import static scene geometry one-to-one with blender. However, I want a dynamic
+ * scene with individual rigidbody transformations affecting their respective children. This is a
+ * complicated task and doesn't seem all that necessary for simplistic simulative environments.
+ *
+ * (Dynamically Build Shaders) 7. Build checksums for various shaders. Index these and reuse them
+ * behind the scenes to save on memory. Very vague statements here but the general idea is to generate
+ * modular WGPU Rust codeblocks to feed into the shading compiler at run time.
+ *
+ * (Build Animation Skeletons) 8. Have the ability to import spline animations and map them to scene graph
+ * information. This also means mesh-skinning implementations need to be implemented, and implemented
+ * efficiently if we want to use them large scale.
+ * -DC @ 10/30/23
+ *
+ */
+
+
 // build a repository of all meshes stored in the gltf file.
 	constructor(device, queue, data) {
-		console.log(data);
 		const mesh_graph = data.graph.meshes;
 // build a repository of meshes
 		const mesh_registry = new object_list(new uid_handler(), {});
@@ -111,11 +187,11 @@ export class scene_context {
 
 		for(const image_type of image_graph) {
 			image_registry.write_obj((args) => new scene_image(args), {
-				name:	image_type.name,
-				mime_type: image_type.mimeType,
-				buffer_view_index: image_type.bufferView,
-				graph: 	data.graph,
-				bins: 	data.bins,
+				name:				image_type.name,
+				mime_type: 			image_type.mimeType,
+				buffer_view_index: 	image_type.bufferView,
+				graph: 				data.graph,
+				bins: 				data.bins,
 				on_loaded: on_image_loaded
 			});
 		} on_image_loaded();
@@ -144,77 +220,28 @@ export class scene_context {
 	get_mesh(index) { return this.#_mesh_registry.get_obj(index); }
 }
 
-// constructs the global transformation matrices for all of the nodes in a glTF tree.
-export const compute_scene_hierarchy=(data, scene, yoink=(node, mesh)=>{})=> {
-// build local matrix for this gltf node
-	const build_trs=(root)=> {
-		const t = root.translation;
-		const r = root.rotation;
-		const s = root.scale;
-		
-		let m = m4f.identity();
-		if(s != undefined) { 
-			s.push(1);
-// maintain type consistency. m4f is expecting a v4f, not v3f.
-			m = m4f.multiply(m4f.diag(s), m);
-		}
-		if(r != undefined) {
-			m = m4f.multiply(q4f.to_m4f(r), m);
-		}
-		if(t != undefined) { 
-			t.push(1);
-// maintain type consistency. m4f is expecting a v4f, not v3f.
-			m = m4f.multiply(m4f.shift(t), m);
-		}
-		return m; // TRS := Translation * Rotation * Scale
-	}
+// a data storage classed used to bundle bitmaps with their samplers
+// going forwards, a texture is a composition of a sampler and its 
+// underlying bitmap -DC @ 10/29/23
+export class scene_texture {
+	#_uid; #_image; #_sampler;
 
-	const nodes = data.graph.nodes;
-	const node_queue = new object_queue();
+	constructor(args) {
+		const props = args.props;
+	
+		const sampler_index = props.sampler_index;
+		const source_index = props.source_index;
 
-// get highest layer of nodes in scene and concatenate to queue in
-// order to initiate breadth first query
-	for(let i=0;i<scene.nodes.length;i++) {
-		const node_index = scene.nodes[i];
-		const child_node = { 
-			index: node_index,
-			matrix: build_trs(nodes[node_index]),
-		};
-		yoink(child_node, nodes[node_index].mesh);
-		node_queue.push(child_node);
-	}
 
-// avoid stack-recursion and use breadth-first bleed instead:
-	while(node_queue.count() > 0) {
-		const node_obj = node_queue.pop();
-		const index = node_obj.index;
-// parent transform
-		const matrix = node_obj.matrix;
-		const node = nodes[index];
 
-		const children = node.children;
-		if(node.children == null) continue;
-
-// append children for concatenation
-		for(let i=0;i<children.length;i++) {
-			const child_index = children[i];
-// local transform
-			const child_matrix = build_trs(nodes[child_index]);
-			const child_node = {
-				index: child_index,
-				matrix: m4f.multiply(matrix, child_matrix)
-			}
-
-			yoink(child_node, nodes[child_index].mesh);
-			node_queue.push(child_node);
-		}
+		this.#_uid = args.uid;
 	}
 }
 
 // a data storage class that helps convert glb-images to
 // webgpu textures -DC @ 10/26/23
 export class scene_image {
-	#_bitmap; #_texture;
+	#_bitmap; #_handler;
 	#_loaded; #_uid; #_name;
 
 	constructor(args) {
@@ -228,7 +255,8 @@ export class scene_image {
 
 // the storage description of this image: { binary index, length, offset } 
 		const image_view = buffer_views[props.buffer_view_index];
-		const image_blob = new Blob([new DataView(
+		const image_blob = new Blob([
+			new DataView(
 				bins[image_view.buffer],
 				image_view.byteOffset, image_view.byteLength
 			)], { type: props.mime_type }
@@ -259,7 +287,7 @@ export class scene_image {
 			return;
 		}
 
-		this.#_texture = gfx.upload_bitmap(device, queue, this.#_bitmap);
+		this.#_handler = gfx.upload_bitmap(device, queue, this.#_bitmap);
 	}
 
 	loaded() { return this.#_loaded; }
@@ -277,6 +305,8 @@ export class scene_mesh {
 		const primitive = props.primitive;
 		const graph = props.graph;
 		const bins = props.bins;
+
+		const materials = graph.materials;
 
 		const buffer_views = graph.bufferViews;
 		const accessors = graph.accessors;
@@ -299,6 +329,12 @@ export class scene_mesh {
 	
 			mesh_attr[obj] = attr;
 		}
+// handle materials
+		if(primitive.material != null) {
+			this['material'] = materials[primitive.material];
+			console.log(this['material']);
+		}
+
 // handle indices
 		if(primitive.indices != null) {
 			const indices_id = primitive.indices;
@@ -375,3 +411,69 @@ export class scene_mesh {
 	uid() { return this.#_uid; }
 }
 
+// constructs the global transformation matrices for all of the nodes in a glTF tree.
+export const compute_scene_hierarchy=(data, scene, yoink=(node, mesh)=>{})=> {
+// build local matrix for this gltf node
+	const build_trs=(root)=> {
+		const t = root.translation;
+		const r = root.rotation;
+		const s = root.scale;
+		
+		let m = m4f.identity();
+		if(s != undefined) { 
+			s.push(1);
+// maintain type consistency. m4f is expecting a v4f, not v3f.
+			m = m4f.multiply(m4f.diag(s), m);
+		}
+		if(r != undefined) {
+			m = m4f.multiply(q4f.to_m4f(r), m);
+		}
+		if(t != undefined) { 
+			t.push(1);
+// maintain type consistency. m4f is expecting a v4f, not v3f.
+			m = m4f.multiply(m4f.shift(t), m);
+		}
+		return m; // TRS := Translation * Rotation * Scale
+	}
+
+	const nodes = data.graph.nodes;
+	const node_queue = new object_queue();
+
+// get highest layer of nodes in scene and concatenate to queue in
+// order to initiate breadth first query
+	for(let i=0;i<scene.nodes.length;i++) {
+		const node_index = scene.nodes[i];
+		const child_node = { 
+			index: node_index,
+			matrix: build_trs(nodes[node_index]),
+		};
+		yoink(child_node, nodes[node_index].mesh);
+		node_queue.push(child_node);
+	}
+
+// avoid stack-recursion and use breadth-first bleed instead:
+	while(node_queue.count() > 0) {
+		const node_obj = node_queue.pop();
+		const index = node_obj.index;
+// parent transform
+		const matrix = node_obj.matrix;
+		const node = nodes[index];
+
+		const children = node.children;
+		if(node.children == null) continue;
+
+// append children for concatenation
+		for(let i=0;i<children.length;i++) {
+			const child_index = children[i];
+// local transform
+			const child_matrix = build_trs(nodes[child_index]);
+			const child_node = {
+				index: child_index,
+				matrix: m4f.multiply(matrix, child_matrix)
+			}
+
+			yoink(child_node, nodes[child_index].mesh);
+			node_queue.push(child_node);
+		}
+	}
+}
